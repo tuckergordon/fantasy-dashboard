055ee6492fd5c4d8d1802ad986e357b6dbb380b9
Tucker Gordon <tucker.gordon18@gmail.com>
2024-10-14 08:50:06 -0400

    Bugfix: incorrect relative api route

--- web/src/routes/[league_id]/+layout.ts
+++ web/src/routes/[league_id]/+layout.ts
@@ -1,7 +1,7 @@
 import { getStandings } from '$lib/sleeper-utils.js';
 
 export async function load({ fetch, params }) {
-	const response = await fetch(`api/leagues/${params.league_id}`);
+	const response = await fetch(`/api/leagues/${params.league_id}`);
 	const { metadata, posts } = await response.json();
 
 	const standings = await getStandings(fetch, params.league_id);
