c8e6d45db4dfb7ff39bcf73dc371acaa57e2bfb0
Tucker Gordon <tucker.gordon18@gmail.com>
2024-11-03 12:14:50 -0500

    Rename: league_id -> leagueId

--- web/src/routes/[leagueId]/posts/[year]/[slug]/+page.ts
+++ web/src/routes/[leagueId]/posts/[year]/[slug]/+page.ts
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 export async function load({ params }) {
   try {
     const post = await import(
-      `../../../../../leagues/${params.league_id}/posts/${params.year}/${params.slug}.md`
+      `../../../../../leagues/${params.leagueId}/posts/${params.year}/${params.slug}.md`
     );
 
     return {
