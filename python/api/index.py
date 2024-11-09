566e5138238ce37bef72ff160db53637c6187f34
Tucker Gordon <tucker.gordon18@gmail.com>
2024-11-09 08:24:37 -0500

    Add python api

---
+++ python/api/index.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
