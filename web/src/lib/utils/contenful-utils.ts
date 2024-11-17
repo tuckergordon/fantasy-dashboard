754c538c4d2d6e505f1c435fb85244163bf23410
Tucker Gordon <gordon_tucker@bah.com>
2024-11-16 18:30:47 -0600

    Add contenful preview content
    When running in development, shows the preview content from contentful

--- web/src/lib/utils/contenful-utils.ts
+++ web/src/lib/utils/contenful-utils.ts
@@ -1,14 +1,16 @@
-import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } from '$env/static/private';
+// prettier-ignore
+import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_PREVIEW_ACCESS_TOKEN } from '$env/static/private';
 import type { ContentfulRecap } from '$lib/models/Contentful.model';
 import { error } from '@sveltejs/kit';
 import { createClient } from 'contentful';
 
 console.log('Creating Contentful client');
 const client = createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
   space: CONTENTFUL_SPACE_ID,
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: CONTENTFUL_ACCESS_TOKEN,
+  // Use the preview access token and host if we're in development so that the unpublished
+  // content is visible
+  accessToken: import.meta.env.DEV ? CONTENTFUL_PREVIEW_ACCESS_TOKEN : CONTENTFUL_ACCESS_TOKEN,
+  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
 });
 
 export async function getPost(leagueId: string, slug: string) {
