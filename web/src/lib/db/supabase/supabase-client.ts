52266422e795863ef2cc9a261985f6b643170bd0
Tucker Gordon <tucker.gordon18@gmail.com>
2024-12-07 11:23:00 -0500

    Use SUPABASE_URL instead of SUPABASE_PROJECT_ID

--- web/src/lib/db/supabase/supabase-client.ts
+++ web/src/lib/db/supabase/supabase-client.ts
@@ -1,8 +1,8 @@
-import { SUPABASE_PROJECT_ID, SUPABASE_ANON_KEY } from '$env/static/private';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 import type { Database } from './supabase.model';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient<Database>(
-  `https://${SUPABASE_PROJECT_ID}.supabase.co`,
+  SUPABASE_URL,
   SUPABASE_ANON_KEY,
 );
